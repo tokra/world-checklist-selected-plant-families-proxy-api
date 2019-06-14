097c499725e4ba0fdefbf772a7e4cfd246909ccc
to-kra <code@tokra.io>
2019-06-14 14:12:29 +0200

    fixes & refactor

--- controllers/searchPlantsController.js
+++ controllers/searchPlantsController.js
@@ -1,35 +1,60 @@
+const config = require('config');
+const _ = require('lodash');
 const request = require('superagent');
-const serverUrl = 'http://apps.kew.org/';
-const url = 'http://wcsp.science.kew.org/qsearch.do';
-const cheerio = require('cheerio');
-
+const { load: cheerioLoad } = require('cheerio');
+const beautify = require('js-beautify');
 const StringUtils = require('../lib/stringUtils');
 
-/**
- * This handle case like: Haworthia retusa
- * Does not work for single: Astrophytum
- * @param {String} plantName 
- */
-const getPlants = async plantName => {
-	const response = await Promise.resolve(doPostPlants(plantName));
-	const html = response.text;
-	let $ = cheerio.load(html, {
+const serverUrl = config.get('serverUrl');
+const url = config.get('apiUrl');
+
+const htmlPrettify = uglyHtml => {
+	const defaults = {
+		unformatted: ['code', 'pre', 'em', 'strong', 'span'],
+		indent_inner_html: true,
+		indent_char: ' ',
+		indent_size: 2,
+		sep: '\n'
+	};
+	const prettyHtml = beautify.html(uglyHtml, defaults);
+	return prettyHtml;
+};
+
+const doPostPlants = async plantName => {
+	try {
+		const result = await request
+			.post(url)
+			.type('form')
+			.send(`plantName=${plantName}`)
+			// .set('Accept-Encoding', 'gzip')
+			.set('Cache-Control', 'no-cache')
+			.timeout({
+				response: 5000, // Wait 5 seconds for the server to start sending,
+				deadline: 30000 // but allow 30sec minute for the file to finish loading.
+			})
+			.redirects(2);
+		return result;
+	} catch (err) {
+		// console.log(err.message);
+		throw err;
+	}
+};
+
+const parseData = htmlString => {
+	/* load html string as cheerio object */
+	const $ = cheerioLoad(htmlString, {
 		normalizeWhitespace: true,
 		xmlMode: true
 	});
-	const dataHtml = $('.field__item').html();
-	const new$ = cheerio.load(dataHtml);
-	const data = new$('a');
-	const dataArray = Object.keys(data).reduce((array, key, index) => {
-		const value = data[key];
-		if (isNaN(parseInt(key))) {
-			return array;
-		}
-		array.push(value);
-		return array;
-	}, []);
-	const finalDataArray = dataArray.reduce((final, p) => {
-		const childElements = p.children.reduce((finalArray, actualElement) => {
+
+	/*  data are under main id, and its in <a> tags */
+	const data = $('#main')
+		.find('a')
+		.toArray();
+
+	/* Parsing */
+	const finalDataArray = data.reduce((final, element) => {
+		const childElements = element.children.reduce((finalArray, actualElement) => {
 			if (actualElement.type === 'text') {
 				const elementText = actualElement.nodeValue.trim();
 				finalArray.push(elementText);
@@ -40,29 +65,39 @@ const getPlants = async plantName => {
 			return finalArray;
 		}, []);
 		const jointOfElements = childElements.join(' ');
-		const href = p.attribs.href;
+		const { href } = element.attribs;
 		final.push({ name: jointOfElements, href });
 		return final;
 	}, []);
-	const resultArray = finalDataArray.map((element, index) => {
-		const fullHref = serverUrl + StringUtils.subStringAfter(element.href, '/');
+
+	/* Full URL mapping */
+	const resultArray = finalDataArray.map(element => {
+		let fullHref;
+		if (_.endsWith(serverUrl, '/')) {
+			fullHref = serverUrl + StringUtils.subStringAfter(element.href, '/');
+		} else {
+			fullHref = serverUrl + element.href;
+		}
 		return { plantName: element.name, moreDetails: fullHref };
 	});
+
 	return resultArray;
 };
 
-const doPostPlants = async plantName => {
-	return await request
-		.post(url)
-		.type('form')
-		.send(`plantName=${plantName}`)
-		//.set('Accept-Encoding', 'gzip')
-		.set('Cache-Control', 'no-cache')
-		.timeout({
-			response: 5000, // Wait 5 seconds for the server to start sending,
-			deadline: 30000 // but allow 30sec minute for the file to finish loading.
-		})
-		.redirects(2);
+/**
+ * This handle case like: Haworthia or Haworthia retusa
+ * Does not work for: Astrophytum
+ * @param {String} plantName
+ */
+const getPlants = async plantName => {
+	try {
+		const response = await Promise.resolve(doPostPlants(plantName));
+		const { text: html } = response;
+		const result = parseData(html);
+		return result;
+	} catch (e) {
+		return { Error: e.message };
+	}
 };
 
 module.exports = {
--- routes/searchPlantsRoutes.js
+++ routes/searchPlantsRoutes.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const router = express.Router();
 const SearchPlantsController = require('../controllers/searchPlantsController');
 
-/* GET users listing. */
-const getPlants = async express => {
-	express.route('/v1/searchPlants').get(handle);
-};
-
-async function handle(req, res, next) {
+async function handle(req, res) {
 	const data = await SearchPlantsController.getPlants(req.query.q);
 	const json = JSON.stringify(data, null, 2);
 	res.setHeader('Content-Type', 'application/json');
 	res.send(json);
 }
+
+/* GET users listing. */
+const getPlants = express => {
+	express.route('/v1/searchPlants').get(handle);
+};
 
 module.exports = {
 	getPlants
